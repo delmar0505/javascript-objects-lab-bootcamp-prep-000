12e36f0367c27ee092e7cd1b029a168dc518b864
oscar leon <first.liberal.thinker@gmail.com>
2018-09-11 16:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var recipes = {object:'value'};
 
-function updateObjectWithKeyAndValue(object, key, value){
-  
-  
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
 }
