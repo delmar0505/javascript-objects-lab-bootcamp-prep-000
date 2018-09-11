303220671270303260a198c943aacc56bc584238
oscar leon <first.liberal.thinker@gmail.com>
2018-09-11 17:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var recipes = {object:'value'};
 
+
+/*
 var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},obj,{[key]:value})
@@ -15,4 +17,21 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
   return object
+}  */
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+ function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return newObj;
+}
+ function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
 }
