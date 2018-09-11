7e31154c60db1478f4c8838e4cfa4a33fe5da881
oscar leon <first.liberal.thinker@gmail.com>
2018-09-11 18:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function deleteFromObjectByKey(object, key) {
   delete object.key;
   return obj
 }
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
-}  
-
  function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj.key;
   return newObj;
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}  
+
+
